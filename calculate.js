3ce50d4679513be949400f3cbc981a40fab1a940
e-mm-u <uh.habiba1012@gmail.com>
2022-08-22 12:36:46 +0600

    checking for valid input functionality added

--- calculate.js
+++ calculate.js
@@ -1,16 +1,26 @@
+// function to clear a field 
 function clearField(id){
     document.getElementById(id).value = '';
 }
 
+// function to get the value written in an input element as a floating number 
 function value(id){
     const value = parseFloat(document.getElementById(id).value);
+
+    if(isNaN(value)){
+        alert('Enter a valid number');
+        return 0;
+    }
+    
     return value;
 }
 
+// function to change inner text of an element 
 function changeInnertext(id,replacewith){
     document.getElementById(id).innerText = replacewith;
 }
 
+// funtion to count child 
 function coundChild(id){
     return document.getElementById(id).children.length;
 }
@@ -18,8 +28,8 @@ function coundChild(id){
 document.getElementById('calculate').addEventListener('click',function(){
 
     const perPersonCost = value('perPlayerCost');
-
     // clearField('perPlayerCost');
+
     const n = coundChild('top-five');
     const totalPerPlayerCost = n * perPersonCost;
     changeInnertext('playerExpensetotal',totalPerPlayerCost);
@@ -37,7 +47,6 @@ document.getElementById('calculate-total').addEventListener('click', function(){
 
     const managerCost = value('manager');
     const coachCost = value('coach');
-
 
     const finalCost = totalPerPlayerCost + managerCost + coachCost;
 
--- topFive.js
+++ topFive.js
@@ -1,34 +1,45 @@
+// function to check how many childs an element has by it's id 
 function coundChild(id){
     return document.getElementById(id).children.length;
 }
 
+// function to create an html element
 function createList(name){
     let li = document.createElement('li');
     li.textContent = name;
     return li;
 }
 
+// here we will fetch all the buttons first by their class name 
 const buttons = document.getElementsByClassName('btn');
 const btnLen = buttons.length;
 
+
+// for each button we need to add event 
 for(let i = 0; i<btnLen; i++){
+
+    // --------------event listener function ------------
     buttons[i].addEventListener('click', function(){
 
-        const n = coundChild('top-five');
-        const top5 = document.querySelector('#top-five');
-        const name = buttons[i].parentElement.children[0].innerHTML;
+        const n = coundChild('top-five');//checking how many children are there
+        const top5 = document.querySelector('#top-five');//fetching the top5 list element
+        const name = buttons[i].parentElement.children[0].innerHTML;//fetching the player name
         
-        const id = buttons[i].parentElement.children[2].id;
-        const element = document.getElementById(id);
+        const id = buttons[i].parentElement.children[2].id; //getting the id of selected button
+        const element = document.getElementById(id);//getting the whole button elemwnt by it's id
+
+        //-------------- button disabled functionality--------------------------
+        // need to add class disabled for bootstrap button 
+        // and bootstrap says to add area-disabled true for the buttons made by a tag 
+
         element.setAttribute("aria-disabled","true");
         element.classList.add("disabled");
-        console.log(name);
+        // console.log(name);
 
 
-
+        // -----list can have at most 5 children, so chcking here before append any child----
         if(n<5){
             top5.appendChild(createList(name));
-            buttons[i].disabled = true;
         }
         else{
             alert("You can put only 5 athletes in your list!");
